93a3cc9ce1c75d9a9471cb0fb248a6ad8e1990f0
John Lonetree <jlonetree@gmail.com>
2020-07-15 03:39:33 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -3,7 +3,7 @@ countdown = 10
 while countdown > 0 do 
   break if countdown == new_year
     puts "#{countdown}"
-  elsif new_year
+  elsif countdown = 0 
     puts "Happy New Year!"
   countdown = countdown - 1
 end 
